314bae7d7fe1399935cf8359375d516aea0324d5
Damian Jeleń <damian.jelen.1986@gmail.com>
2022-12-02 10:39:37 +0100

    js read task from api, create task in api

--- js/script.js
+++ js/script.js
@@ -1,3 +1,47 @@
 const apikey = "37b5e7e3-6711-46ec-91fd-b84438a96cf1";
 const apihost = 'https://todo-api.coderslab.pl';
 
+function apiListTasks() {
+    return fetch(
+        apihost + '/api/tasks',
+        {
+            headers: { Authorization: apikey }
+        }
+    ).then(
+        function(resp) {
+            if(!resp.ok) {
+                alert('Wystąpił błąd! Otwórz devtools i zakładkę Sieć/Network, i poszukaj przyczyny');
+            }
+            return resp.json();
+        });
+}
+
+function apiCreateTask(title, description) {
+    return fetch(
+        apihost + '/api/tasks',
+        {
+            headers: {Authorization: apikey, 'Content-Type': 'application/json'},
+            body: JSON.stringify({title: title, description: description, status: 'open'}),
+            method: 'POST'
+        })
+        .then(function (resp) {
+            if (!resp.ok) {
+                alert('Wystąpił błąd!!!');
+            }
+            return resp.json();
+        });
+}
+
+apiListTasks().then((response) => {
+    response.data.forEach(obj => console.log(obj.title));
+})
+
+
+
+
+
+
+
+
+
+
